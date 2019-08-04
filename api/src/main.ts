859deb1757ef797df061424c73842b522d11c0aa
Jian Han <jian.han@mail.com>
2019-08-04 20:09:01 +1000

    [fix] added compression

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService, getConfigService } from './config/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 
 async function bootstrap(config: ConfigService) {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(compression());
   await app.listen(config.get('APP_PORT'));
 }
 bootstrap(getConfigService());
