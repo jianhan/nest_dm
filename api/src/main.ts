4e6e2b67ec2a13a4b0c4dfb13af2461f6134ee77
Jian Han <jian.han@mail.com>
2019-08-04 22:44:25 +1000

    [feat] added middlewares

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,11 +3,23 @@ import { AppModule } from './app.module';
 import { ConfigService, getConfigService } from './config/config.service';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap(config: ConfigService) {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(compression());
+  app.use(helmet());
+  // app.enableCors();
+  // app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   await app.listen(config.get('APP_PORT'));
 }
 bootstrap(getConfigService());
