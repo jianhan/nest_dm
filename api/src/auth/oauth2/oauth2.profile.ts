0a9148001858b65d29f3729512b075c44b6d9010
Jian Han <jian.han@mail.com>
2019-08-05 18:06:57 +1000

    [doc] added more comment for oauth2 profile

--- api/src/auth/oauth2/oauth2.profile.ts
+++ api/src/auth/oauth2/oauth2.profile.ts
@@ -1,13 +1,82 @@
 import { Oauth2Provider } from './constants';
 
+/**
+ * Oauth2Profile is a generic oauth2 profile data structure, all oauth2
+ * provider profiles will be converted into this structure.
+ *
+ * @export
+ * @interface Oauth2Profile
+ */
 export interface Oauth2Profile {
-  email?: string;
-  avatarUrl?: string;
-  firstName?: string;
-  lastName?: string;
+  /**
+   * providerId is unique identifier of profile from provider.
+   *
+   * @type {number}
+   * @memberof Oauth2Profile
+   */
   profileId: number;
+
+  /**
+   * provider represents oauth2 provider.
+   *
+   * @type {Oauth2Provider}
+   * @memberof Oauth2Profile
+   */
   provider: Oauth2Provider;
+
+  /**
+   * displayName is the display name from oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
   displayName: string;
+
+  /**
+   * username is username from oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
   username: string;
+
+  /**
+   * profileUrl is url of oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
   profileUrl: string;
+
+  /**
+   * email is email of oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
+  email?: string;
+
+  /**
+   * avatarUrl is url of avatar of oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
+  avatarUrl?: string;
+
+  /**
+   * firstName is first name of oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
+  firstName?: string;
+
+  /**
+   * lastName is last name of oauth2 profile.
+   *
+   * @type {string}
+   * @memberof Oauth2Profile
+   */
+  lastName?: string;
 }
